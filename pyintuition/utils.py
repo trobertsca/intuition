1e550c289c23fc8300c382f6875a4312ddd76a55
Timothy Roberts <timothyroberts.ca@gmail.com>
2018-03-20 15:14:31 -0700

    Small change to prefix_field_names.

--- pyintuition/utils.py
+++ pyintuition/utils.py
@@ -1,9 +1,9 @@
 def prefix_field_names(record: dict) -> dict:
     """Prefix fields with '_fnm_' for QuickBase."""
+    _record = {}
     for field in record.keys():
         # Don't prefix rid, it isn't a field name for us
         if field == 'rid':
             pass
-        
-        record[f'_fnm_{field}'] = record.pop(field)
-        return record
+        _record[f'_fnm_{field}'] = record[field]
+        return _record
